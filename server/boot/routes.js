c37d29939a7f9705ae24cdb1150979e07cfab8e8
armova <a3@3vot.com>
2015-02-17 23:13:31 -0600

    welcome text changed

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
 
   app.get('/welcome', function(req, res) {
-    res.send('Bienvenido! Email confirmado, por favor ir a la página principal picmap.3votapp.com');
+    res.send('Listo! Email confirmado, por favor ir a la <a href="http://picmap.3votapp.com/">página principal</a>');
   });
 
 }
